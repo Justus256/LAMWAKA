d0ffcf1b868039a28e2b096dced9fea65645acbe
Justus256 <nsabajustus12@gmail.com>
2025-02-19 14:39:21 +0300

    melatpp.py port changes

--- melatpp.py
+++ melatpp.py
@@ -249,7 +249,7 @@ def update_weekly_summary(n_clicks):
     return px.bar(title="No data")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run_server(host='0.0.0.0', port=port)
 
                        
